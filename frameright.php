40ad3cc5ee4f3962fd00e73c904f80f538939d69
Aurelien Lourot <software@lourot.dev>
2022-09-23 17:18:15 +0200

    Fill plugin header

--- frameright.php
+++ frameright.php
@@ -1,3 +1,14 @@
 /**
- * Plugin Name: Frameright
+ * Plugin Name:       Frameright
+ * Plugin URI:        https://frameright.io
+ * Description:       An easy way to leverage image cropping metadata on your site. Power to the pictures!
+ * Author:            Frameright
+ * Author URI:        https://frameright.io
+ * Version:           0.0.1
+ * License:           GPL-3.0-or-later
+ * License URI:       license.txt
+ * Text Domain:       frameright
+ * Domain Path:       /languages
+ * Requires PHP:      5.6
+ * Requires at least: 5.1
  */
