5482181da66a188df04eebbabbaa7d4dff6bea94
Aurelien Lourot <software@lourot.dev>
2022-09-23 16:58:52 +0200

    Add GPLv3 license file

--- frameright.php
+++ frameright.php
@@ -1,3 +1,19 @@
+/*
+Frameright WordPress Plugin is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+Frameright WordPress Plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Frameright WordPress Plugin. If not, see
+https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
 /**
  * Plugin Name:       Frameright
  * Plugin URI:        https://frameright.io
