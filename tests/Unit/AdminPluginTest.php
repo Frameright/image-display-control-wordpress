2985e165966966daadcef304d1250e732d35acc3
Aurelien Lourot <software@lourot.dev>
2022-09-28 17:22:10 +0200

    Add unit test for AdminPlugin

---
+++ tests/Unit/AdminPluginTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\Support\UnitTester;
+
+class AdminPluginTest extends \Codeception\Test\Unit {
+    protected UnitTester $tester;
+
+    protected function _before() {
+    }
+
+    // tests
+    public function testSomeFeature() {
+    }
+}
