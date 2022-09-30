4fc67a05440318a179e600f15734107ba6ebd888
Aurelien Lourot <software@lourot.dev>
2022-09-30 21:56:18 +0200

    Improve unique_target_file()

--- admin/admin-plugin.php
+++ admin/admin-plugin.php
@@ -45,6 +45,39 @@ class AdminPlugin {
     const EXTENSION_SEPARATOR = '.';
 
     /**
+     * Split name and extension from a file name.
+     *
+     * @param string $basename File name, e.g. 'my.great.file.jpg'.
+     * @return array ['my.great.file', 'jpg'] .
+     */
+    private static function basename_to_name_and_extension($basename) {
+        $basename_items = explode(self::EXTENSION_SEPARATOR, $basename);
+        Debug\assert_(
+            count($basename_items) >= 2,
+            "'$basename' should contain at least one '" .
+                self::EXTENSION_SEPARATOR .
+                "'"
+        );
+
+        $extension = array_pop($basename_items);
+        $name = implode(self::EXTENSION_SEPARATOR, $basename_items);
+
+        return [$name, $extension];
+    }
+
+    /**
+     * Create hardcropped versions of a given source image.
+     *
+     * @param string $source_image_path Absolute path of the source image.
+     */
+    private function create_hardcrops($source_image_path) {
+        Debug\log("Creating hardcrops of $source_image_path ...");
+
+        $target_image_file = $this->unique_target_file($source_image_path);
+        Debug\log('Target file: ' . print_r($target_image_file, true));
+    }
+
+    /**
      * Generate a non-existing file name/path for creating a copy/variant of a
      * given source file.
      *
@@ -56,29 +89,35 @@ class AdminPlugin {
      *               * 'name':      'img-frameright'
      *               * 'extension': 'jpg'
      */
-    private static function unique_target_file($source_path) {
+    private function unique_target_file($source_path) {
         $source_basename = basename($source_path);
         $source_dirname = dirname($source_path);
 
-        $source_basename_items = explode(
-            self::EXTENSION_SEPARATOR,
+        $source_basename_items = self::basename_to_name_and_extension(
             $source_basename
         );
-        Debug\assert_(
-            count($source_basename_items) >= 2,
-            "'$source_basename' should contain at least one '" .
-                self::EXTENSION_SEPARATOR .
-                "'"
-        );
-        $source_extension = array_pop($source_basename_items);
-        $source_name = implode(
-            self::EXTENSION_SEPARATOR,
-            $source_basename_items
-        );
+        $source_name = $source_basename_items[0];
+        $source_extension = $source_basename_items[1];
 
         $target_name = $source_name . '-frameright';
         $target_basename =
             $target_name . self::EXTENSION_SEPARATOR . $source_extension;
+
+        // In case this file already exists, ask WordPress to find a new
+        // filename in that same folder that doesn't exist yet.
+        // Note: will be empty if $target_basename was already unique, see
+        // https://developer.wordpress.org/reference/functions/wp_unique_filename/
+        $target_basename_unique = $this->global_functions->wp_unique_filename(
+            $source_dirname,
+            $target_basename
+        );
+        $target_basename = $target_basename_unique
+            ? $target_basename_unique
+            : $target_basename;
+        $target_name = self::basename_to_name_and_extension(
+            $target_basename
+        )[0];
+
         $target_path = $source_dirname . DIRECTORY_SEPARATOR . $target_basename;
 
         $result = [
@@ -90,18 +129,6 @@ class AdminPlugin {
         ];
 
         return $result;
-    }
-
-    /**
-     * Create hardcropped versions of a given source image.
-     *
-     * @param string $source_image_path Absolute path of the source image.
-     */
-    private function create_hardcrops($source_image_path) {
-        Debug\log("Creating hardcrops of $source_image_path ...");
-
-        $target_image_file = self::unique_target_file($source_image_path);
-        Debug\log('Target file: ' . print_r($target_image_file, true));
     }
 
     /**
--- tests/AdminPluginTest.php
+++ tests/AdminPluginTest.php
@@ -11,23 +11,49 @@ require_once __DIR__ . '/../admin/admin-plugin.php';
  * Tests for Frameright\Admin\AdminPlugin .
  */
 final class AdminPluginTest extends PHPUnit\Framework\TestCase {
+    // phpcs:disable PHPCompatibility.FunctionDeclarations.NewReturnTypeDeclarations.voidFound
+    /**
+     * Test setup.
+     */
+    protected function setUp(): void {
+        $this->global_functions_mock = $this->getMockBuilder(stdClass::class)
+            ->addMethods(['add_filter', 'wp_unique_filename'])
+            ->getMock();
+    }
+    // phpcs:enable
+
     /**
      * Test constructor.
      */
     public function test_constructor() {
-        $global_functions_mock = $this->getMockBuilder(stdClass::class)
-            ->addMethods(['add_filter'])
-            ->getMock();
-        $global_functions_mock
+        $this->global_functions_mock
             ->expects($this->once())
             ->method('add_filter')
             ->with('wp_handle_upload');
 
-        new Frameright\Admin\AdminPlugin($global_functions_mock);
+        new Frameright\Admin\AdminPlugin($this->global_functions_mock);
     }
 
     /**
-     * Test AdminPlugin::unique_target_file().
+     * Test basename_to_name_and_extension().
+     */
+    public function test_basename_to_name_and_extension() {
+        $input_basename = 'my.great.file.jpg';
+
+        $expected_result = ['my.great.file', 'jpg'];
+
+        $method = new ReflectionMethod(
+            'Frameright\Admin\AdminPlugin',
+            'basename_to_name_and_extension'
+        );
+        $method->setAccessible(true);
+        $actual_result = $method->invoke(null, $input_basename);
+
+        $this->assertSame($expected_result, $actual_result);
+    }
+
+    /**
+     * Test unique_target_file().
      */
     public function test_unique_target_file() {
         $input_source_path = '/absolute/path/to/img.jpg';
@@ -40,13 +66,28 @@ final class AdminPluginTest extends PHPUnit\Framework\TestCase {
             'extension' => 'jpg',
         ];
 
+        $this->global_functions_mock
+            ->expects($this->once())
+            ->method('wp_unique_filename')
+            ->with('/absolute/path/to', 'img-frameright.jpg');
+
         $method = new ReflectionMethod(
             'Frameright\Admin\AdminPlugin',
             'unique_target_file'
         );
         $method->setAccessible(true);
-        $actual_result = $method->invoke(null, $input_source_path);
+        $actual_result = $method->invoke(
+            new Frameright\Admin\AdminPlugin($this->global_functions_mock),
+            $input_source_path
+        );
 
         $this->assertSame($expected_result, $actual_result);
     }
+
+    /**
+     * GlobalFunctions mock.
+     *
+     * @var Mock_stdClass
+     */
+    private $global_functions_mock;
 }
