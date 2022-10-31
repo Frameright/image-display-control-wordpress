38c50dc1ab74a10b50222929beaf151b3fc20320
Aurelien Lourot <software@lourot.dev>
2022-10-31 12:13:49 +0100

    Add script for building the plugin as a zip archive

---
+++ bin/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$COMPOSER_BINARY" = "" ]; then
+    echo "Run me with 'composer build' instead"
+    exit 1
+fi
+
+mkdir -p build/
+tmpdir="$(mktemp -d -p ./build/)"
+echo "Building in $tmpdir ..."
+
+cp -r *.php languages/ license.txt readme.txt src/ "$tmpdir"
+
+target="$(pwd)/frameright.zip"
+rm -f "$target"
+
+cd "$tmpdir"
+zip -r "$target" .
