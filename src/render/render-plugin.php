83730a1f36db0320094fc142f03b5f85c6e7f793
Aurelien Lourot <software@lourot.dev>
2022-12-25 18:32:14 +0100

    LA_TEMP hooks

--- src/render/render-plugin.php
+++ src/render/render-plugin.php
@@ -95,6 +95,305 @@ class RenderPlugin {
                 5 // number of arguments
             );
         }
+
+        $this->global_functions->add_filter(
+            'pre_option_stylesheet',
+            [$this, 'LA_TEMP_pre_option_stylesheet'],
+            10, // default priority
+            3 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'alloptions',
+            [$this, 'LA_TEMP_alloptions'],
+            10, // default priority
+            1 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'option_stylesheet',
+            [$this, 'LA_TEMP_option_stylesheet'],
+            10, // default priority
+            2 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'alloptions',
+            [$this, 'LA_TEMP_stylesheet'],
+            10, // default priority
+            1 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'theme_file_path',
+            [$this, 'LA_TEMP_theme_file_path'],
+            10, // default priority
+            2 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'stylesheet_directory',
+            [$this, 'LA_TEMP_stylesheet_directory'],
+            10, // default priority
+            3 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'locale_stylesheet_uri',
+            [$this, 'LA_TEMP_locale_stylesheet_uri'],
+            10, // default priority
+            2 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'wp_sitemaps_stylesheet_url',
+            [$this, 'LA_TEMP_wp_sitemaps_stylesheet_url'],
+            10, // default priority
+            1 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'wp_sitemaps_stylesheet_index_url',
+            [$this, 'LA_TEMP_wp_sitemaps_stylesheet_index_url'],
+            10, // default priority
+            1 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'theme_root',
+            [$this, 'LA_TEMP_theme_root'],
+            10, // default priority
+            1 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'pre_option_theme_mods_twentytwentythree',
+            [$this, 'LA_TEMP_pre_option_theme_mods_twentytwentythree'],
+            10, // default priority
+            3 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'option_theme_mods_twentytwentythree',
+            [$this, 'LA_TEMP_option_theme_mods_twentytwentythree'],
+            10, // default priority
+            2 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'pre_option_template',
+            [$this, 'LA_TEMP_pre_option_template'],
+            10, // default priority
+            3 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'option_template',
+            [$this, 'LA_TEMP_option_template'],
+            10, // default priority
+            2 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'page_css_class',
+            [$this, 'LA_TEMP_page_css_class'],
+            10, // default priority
+            5 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'page_css_class',
+            [$this, 'LA_TEMP_page_css_class'],
+            10, // default priority
+            5 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'render_block_data',
+            [$this, 'LA_TEMP_render_block_data'],
+            10, // default priority
+            3 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'render_block_core/image',
+            [$this, 'LA_TEMP_render_block_core_image'],
+            10, // default priority
+            3 // number of arguments
+        );
+        $this->global_functions->add_filter(
+            'post_thumbnail_id',
+            [$this, 'LA_TEMP_post_thumbnail_id'],
+            10, // default priority
+            2 // number of arguments
+        );
+    }
+
+    // https://developer.wordpress.org/reference/hooks/pre_option_option/
+    public function LA_TEMP_pre_option_stylesheet(
+        $pre_option,
+        $option,
+        $default
+    ) {
+        Debug\log('LA_TEMP pre_option_stylesheet');
+        Debug\log('  pre_option' . print_r($pre_option, true));
+        Debug\log('  option' . print_r($option, true));
+        Debug\log('  default' . print_r($default, true));
+        return $pre_option;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/alloptions/
+    public function LA_TEMP_alloptions($alloptions) {
+        Debug\log('LA_TEMP alloptions');
+        Debug\log('  alloptions' . print_r($alloptions, true));
+        return $alloptions;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/option_option/
+    public function LA_TEMP_option_stylesheet($value, $option) {
+        Debug\log('LA_TEMP option_stylesheet');
+        Debug\log('  value' . print_r($value, true));
+        Debug\log('  option' . print_r($option, true));
+        return $value;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/stylesheet/
+    public function LA_TEMP_stylesheet($stylesheet) {
+        Debug\log('LA_TEMP stylesheet');
+        Debug\log('  stylesheet' . print_r($stylesheet, true));
+        return $stylesheet;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/theme_file_path/
+    public function LA_TEMP_theme_file_path($path, $file) {
+        Debug\log('LA_TEMP theme_file_path');
+        Debug\log('  path' . print_r($path, true));
+        Debug\log('  file' . print_r($file, true));
+        return $path;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/stylesheet_directory/
+    public function LA_TEMP_stylesheet_directory(
+        $stylesheet_dir,
+        $stylesheet,
+        $theme_root
+    ) {
+        Debug\log('LA_TEMP stylesheet_directory');
+        Debug\log('  stylesheet_dir' . print_r($stylesheet_dir, true));
+        Debug\log('  stylesheet' . print_r($stylesheet, true));
+        Debug\log('  theme_root' . print_r($theme_root, true));
+        return $stylesheet_dir;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/locale_stylesheet_uri/
+    public function LA_TEMP_locale_stylesheet_uri(
+        $stylesheet_uri,
+        $stylesheet_dir_uri
+    ) {
+        Debug\log('LA_TEMP locale_stylesheet_uri');
+        Debug\log('  stylesheet_uri' . print_r($stylesheet_uri, true));
+        Debug\log('  stylesheet_dir_uri' . print_r($stylesheet_dir_uri, true));
+        return $stylesheet_uri;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/wp_sitemaps_stylesheet_url/
+    public function LA_TEMP_wp_sitemaps_stylesheet_url($sitemap_url) {
+        Debug\log('LA_TEMP wp_sitemaps_stylesheet_url');
+        Debug\log('  sitemap_url' . print_r($sitemap_url, true));
+        return $sitemap_url;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/wp_sitemaps_stylesheet_index_url/
+    public function LA_TEMP_wp_sitemaps_stylesheet_index_url($sitemap_url) {
+        Debug\log('LA_TEMP wp_sitemaps_stylesheet_index_url');
+        Debug\log('  sitemap_url' . print_r($sitemap_url, true));
+        return $sitemap_url;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/theme_root/
+    public function LA_TEMP_theme_root($theme_root) {
+        Debug\log('LA_TEMP theme_root');
+        Debug\log('  theme_root' . print_r($theme_root, true));
+        return $theme_root;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/pre_option_option/
+    public function LA_TEMP_pre_option_theme_mods_twentytwentythree(
+        $pre_option,
+        $option,
+        $default
+    ) {
+        Debug\log('LA_TEMP pre_option_theme_mods_twentytwentythree');
+        Debug\log('  pre_option' . print_r($pre_option, true));
+        Debug\log('  option' . print_r($option, true));
+        Debug\log('  default' . print_r($default, true));
+        return $pre_option;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/option_option/
+    public function LA_TEMP_option_theme_mods_twentytwentythree(
+        $value,
+        $option
+    ) {
+        Debug\log('LA_TEMP option_theme_mods_twentytwentythree');
+        Debug\log('  value' . print_r($value, true));
+        Debug\log('  option' . print_r($option, true));
+        return $value;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/pre_option_option/
+    public function LA_TEMP_pre_option_template(
+        $pre_option,
+        $option,
+        $default
+    ) {
+        Debug\log('LA_TEMP pre_option_template');
+        Debug\log('  pre_option' . print_r($pre_option, true));
+        Debug\log('  option' . print_r($option, true));
+        Debug\log('  default' . print_r($default, true));
+        return $pre_option;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/option_option/
+    public function LA_TEMP_option_template($value, $option) {
+        Debug\log('LA_TEMP option_template');
+        Debug\log('  value' . print_r($value, true));
+        Debug\log('  option' . print_r($option, true));
+        return $value;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/page_css_class/
+    public function LA_TEMP_page_css_class(
+        $css_class,
+        $page,
+        $depth,
+        $args,
+        $current_page_id
+    ) {
+        Debug\log('LA_TEMP page_css_class');
+        Debug\log('  css_class' . print_r($css_class, true));
+        Debug\log('  page' . print_r($page, true));
+        Debug\log('  depth' . print_r($depth, true));
+        Debug\log('  args' . print_r($args, true));
+        Debug\log('  current_page_id' . print_r($current_page_id, true));
+        return $css_class;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/render_block_data/
+    public function LA_TEMP_render_block_data(
+        $parsed_block,
+        $source_block,
+        $parent_block
+    ) {
+        Debug\log('LA_TEMP render_block_data');
+        Debug\log('  parsed_block' . print_r($parsed_block, true));
+        Debug\log('  source_block' . print_r($source_block, true));
+        Debug\log('  parent_block' . print_r($parent_block, true));
+        return $parsed_block;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/render_block_this-name/
+    public function LA_TEMP_render_block_core_image(
+        $block_content,
+        $block,
+        $instance
+    ) {
+        Debug\log('LA_TEMP render_block_core/image');
+        Debug\log('  block_content' . print_r($block_content, true));
+        Debug\log('  block' . print_r($block, true));
+        Debug\log('  instance' . print_r($instance, true));
+        return $block_content;
+    }
+
+    // https://developer.wordpress.org/reference/hooks/post_thumbnail_id/
+    public function LA_TEMP_post_thumbnail_id($thumbnail_id, $post) {
+        Debug\log('LA_TEMP post_thumbnail_id');
+        Debug\log('  thumbnail_id' . print_r($thumbnail_id, true));
+        Debug\log('  post' . print_r($post, true));
+        return $thumbnail_id;
     }
 
     /**
